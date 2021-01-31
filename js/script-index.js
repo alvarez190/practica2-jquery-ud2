0df122277f316a4e39d2f710c20fa030b6b215cb
stalin alvarez <stalinrap_123@hotmail.com>
2021-01-31 21:05:28 +0100

    [ADD]practica 2 UD 2

---
+++ js/script-index.js
@@ -0,0 +1,54 @@
+$('.switcher button').hide();
+
+$(document).ready(function() {
+
+    $('.switcher').hover(function() {
+        // over
+        $(this).css("background-color", "#74F363")
+    }, function() {
+        // out
+        $(this).css("background-color", "#ddd");
+    });
+
+    $('.switcher').click(function(e) {
+        $('.switcher button').show(500);
+
+        $(this).click(function(e) {
+
+            $('.switcher button').hide();
+            e.preventDefault();
+
+        });
+        e.preventDefault();
+    });
+
+    $('button[data-style=default]').click(function(e) {
+        $('main').css({
+            "width": "",
+            "float": "",
+            "font-size": ""
+        });
+        console.log("Default");
+        e.preventDefault();
+    });
+
+    $('button[data-style=narrow]').click(function(e) {
+        $('main').css({
+            "width": "30%",
+            "float": "left",
+            "font-size": ""
+        });
+        console.log("narrow");
+        e.preventDefault();
+    });
+
+    $('button[data-style=large]').click(function(e) {
+        $('main').css({
+            "width": "",
+            "float": "",
+            "font-size": "large"
+        });
+        console.log("large");
+        e.preventDefault();
+    });
+});
